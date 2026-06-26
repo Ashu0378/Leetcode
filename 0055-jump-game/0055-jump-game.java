0531bbfbbf34004b1e551e96a8a739fb0d9bd103
Ashu0378 <197368121+Ashu0378@users.noreply.github.com>
2026-06-26 21:16:07 +0530

    Time: 2 ms (88.85%), Space: 48.1 MB (23.21%) - LeetHub

---
+++ 0055-jump-game/0055-jump-game.java
@@ -0,0 +1,13 @@
+class Solution {
+    public boolean canJump(int[] nums) {
+        if(nums.length==1)return true;
+        int lastPos=nums.length-1;
+        for(int i=nums.length-2;i>=0;i--){
+            if(nums[i]+i>=lastPos){
+                lastPos=i;
+            }
+        }
+        if(lastPos==0) return true;
+        return false;
+    }
+}
