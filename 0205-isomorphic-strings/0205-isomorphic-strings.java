e33d9f460ad6a50997fc4a08194f86dedd6cc766
Ashu0378 <197368121+Ashu0378@users.noreply.github.com>
2026-07-07 11:27:37 +0530

    Time: 6 ms (89.09%), Space: 43.9 MB (58.83%) - LeetHub

---
+++ 0205-isomorphic-strings/0205-isomorphic-strings.java
@@ -0,0 +1,21 @@
+class Solution {
+    public boolean isIsomorphic(String s, String t) {
+        if (s.length()==0 && t.length()==0 && s.length() != t.length()) return false;
+
+        int[] charS=new int[256];
+        int[] charT=new int[256];
+
+        for(int i=0;i<s.length();i++){
+            char s_=s.charAt(i);
+            char t_=t.charAt(i);
+
+            if(charS[s_]!=charT[t_]){
+                return false;
+            }
+            charS[s_]=i+1;
+            charT[t_]=i+1;
+
+        }
+        return true;
+    }
+}
