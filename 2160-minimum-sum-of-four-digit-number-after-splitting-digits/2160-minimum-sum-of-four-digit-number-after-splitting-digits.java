02e5e7f6296ba58f8019f83813aced99a5089e6b
Ashu0378 <197368121+Ashu0378@users.noreply.github.com>
2026-07-28 10:00:50 +0530

    Time: 2 ms (47%), Space: 42.2 MB (72.14%) - LeetHub

--- 2160-minimum-sum-of-four-digit-number-after-splitting-digits/2160-minimum-sum-of-four-digit-number-after-splitting-digits.java
+++ 2160-minimum-sum-of-four-digit-number-after-splitting-digits/2160-minimum-sum-of-four-digit-number-after-splitting-digits.java
@@ -3,15 +3,10 @@ class Solution {
         int[] arr=new int[4];
         int i=0;
         while(num>0){
-            int temp=num%10;
-            arr[i++]=temp;
+            arr[i++]=num%10;
             num/=10;
         }
         Arrays.sort(arr);
-        // if(arr[0]==0 && arr[1]==0 && arr[2]==0) return arr[3];
-        // else if(arr[0]==0 && arr[1]==0) return arr[2]+arr[3];
-        // else if(arr[0]==0) return (arr[1]*10)+arr[2]+arr[3];
-    
         return ((arr[0]*10)+arr[2])+((arr[1]*10)+arr[3]);
         
 
