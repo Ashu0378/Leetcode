93910e1d0b5e8ebcc7c43bb9f7bdcb43abfc5631
Ashu0378 <197368121+Ashu0378@users.noreply.github.com>
2026-09-01 21:26:57 +0530

    Time: 7 ms (96.85%), Space: 62 MB (98.6%) - LeetHub

--- 0904-fruit-into-baskets/0904-fruit-into-baskets.java
+++ 0904-fruit-into-baskets/0904-fruit-into-baskets.java
@@ -1,21 +1,19 @@
 class Solution {
     public int totalFruit(int[] fruits) {
-        HashMap<Integer,Integer> map=new HashMap<>();
+        int[] freq=new int[fruits.length];
         int left=0;
         int unique=0;
         int max=0;
-        int count=0;
         for(int i=0;i<fruits.length;i++){
-            if(!map.containsKey(fruits[i]) || map.get(fruits[i])==0) unique++;
-            map.put(fruits[i],map.getOrDefault(fruits[i],0)+1);
-            count++;
+            if(freq[fruits[i]]==0) unique++;
+            freq[fruits[i]]++;
             while(unique>2){
-                map.put(fruits[left],map.getOrDefault(fruits[left],0)-1);
-                if(map.get(fruits[left])==0) unique--;
+                freq[fruits[left]]--;
+                if(freq[fruits[left]]==0) unique--;
                 left++;
-                count--;
+
             }
-            max=Math.max(max,count);
+            max=Math.max(max,i-left+1);
         }
         return max;
     }
